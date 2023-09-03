db98699e9f92fc696aafe26e3e6d3f9b5c1216e8
Nour Muhammed <nourmuhammed20121994@gmail.com>
2023-09-03 23:14:13 +0300

    change restaurant logo and see again

--- resources/views/restaurant/lteLayout/sidebar.blade.php
+++ resources/views/restaurant/lteLayout/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('restaurant.home') }}" class="brand-link">
-        <img src="{{ asset('/uploads/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('/public/uploads/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light"> @lang('messages.control_panel') </span>
     </a>
