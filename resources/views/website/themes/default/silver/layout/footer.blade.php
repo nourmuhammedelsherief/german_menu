ae148c6f5f3ac53f2c621e96df799607075d3cf3
Nour Muhammed <nourmuhammed20121994@gmail.com>
2023-10-10 22:06:08 +0300

    remove esay site

--- resources/views/website/themes/default/silver/layout/footer.blade.php
+++ resources/views/website/themes/default/silver/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- footer and footer card-->
-<a href="https://easymenu.site/" target="_blank"><p class="footer-copyright pb-3 mb-1 pt-0 mt-0 font-13 font-600"
+<a href="http://menusservice.com/restaurant/login" target="_blank"><p class="footer-copyright pb-3 mb-1 pt-0 mt-0 font-13 font-600"
                                                     style="color: {{(!isset($restaurant->id) or $restaurant->color == null) ? 'orange' : $restaurant->color->main_heads }} !important"
     >@lang('messages.made_love')
         <i class="fa fa-heart font-14 color-red1-dark"></i>
